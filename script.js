a5140e78f04674c04e8492eb9635d936a6c755e2
alejoevilches <alejoevilches@gmail.com>
2022-12-06 15:46:42 -0300

    First version of tic tac toe

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 let jugador=0;
-let pc=2;
+let pc=3;
 
 //1 es piedra, 2 es papel, 3 es tijera
 
 jugador=prompt("1 es piedra, 2 es papel, 3 es tijera")
-console.log(jugador)
+
 if (jugador==1){
-    alert("Perdiste")
+    alert("Elegiste piedra")
 } else if (jugador==2){
-    alert("Empatamos")
+    alert("Elegiste papel")
 } else if (jugador==3){
-    alert("Ganaste")
+    alert("Elegiste tijera")
+} 
+
+if (pc==1){
+    alert("La maquina eligió piedra")
+} else if (pc==2){
+    alert("La maquina eligió papel")
+} else if (pc==3){
+    alert("La maquina eligió tijera")
+}
+
+//COMBATES
+
+if (jugador == pc){
+    alert("EMPATE");
+} else if (jugador==1 && pc==3){
+    alert("GANASTE");
+} else if (jugador==2 && pc==1){
+    alert("GANASTE");
+} else if (jugador==3 && pc==2){
+    alert("GANASTE");
+} else{
+    alert("PERDISTE");
 }
