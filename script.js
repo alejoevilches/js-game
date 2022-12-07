e6a5f3d4753a5414a4a7c1ba561e57de77b99f39
alejoevilches <alejoevilches@gmail.com>
2022-12-07 01:33:01 -0300

    Now the best of 3 games win the match. Refactoring count function

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
+//Inicializo variables
 let jugador
+let victorias = 0
+let derrotas = 0
 
-//Hacer que la PC elija un numero random cada vuelta
+//Funcion para que la PC elija un numero
 function range(min, max){
-    return Math.floor(Math.random()*max+min-1)+min;}
+    return Math.floor(Math.random()*(max-min+1))+min;}
 
-let pc=range(1,3);
-console.log(pc);
-
-//1 es piedra, 2 es papel, 3 es tijera. Mostrar la elección de cada una de las partes.
-
-jugador=prompt("1 es piedra, 2 es papel, 3 es tijera")
-
+//Funcion para que se muestre en pantalla la eleccion de las partes
 function election(val){
     if (val==1){
         return "Piedra";
@@ -19,33 +16,26 @@ function election(val){
     } else if (val==3){
         return "Tijera";
     }
+    }
+
+function count(pc, jugador){
+    if (jugador == pc){
+        alert("EMPATE");
+    } else if ((jugador==1 && pc==3) || (jugador==2 && pc==1) || (jugador==3 && pc==2)){
+        alert("GANASTE");
+        victorias++
+    } else{
+        alert("PERDISTE");
+        derrotas++
+    }
 }
 
-alert("El jugador eligió " + election(jugador))
-alert("La PC eligió " + election(pc))
-
-/*if (jugador==1){
-    alert("Elegiste piedra")
-} else if (jugador==2){
-    alert("Elegiste papel")
-} else if (jugador==3){
-    alert("Elegiste tijera")
-} 
-
-if (pc==1){
-    alert("La maquina eligió piedra")
-} else if (pc==2){
-    alert("La maquina eligió papel")
-} else if (pc==3){
-    alert("La maquina eligió tijera")
-} */
-
-//COMBATES
-
-if (jugador == pc){
-    alert("EMPATE");
-} else if ((jugador==1 && pc==3) || (jugador==2 && pc==1) || (jugador==3 && pc==2)){
-    alert("GANASTE");
-} else{
-    alert("PERDISTE");
+//Hago el bucle para que se repita hasta que gane el mejor de 3
+while (victorias < 3 && derrotas < 3){
+    let pc=range(1,3);
+    let jugador=prompt("1 es piedra, 2 es papel, 3 es tijera");
+    alert("El jugador eligió " + election(jugador))
+    alert("La PC eligió " + election(pc))
+    count(pc, jugador);
 }
+alert("Fin del juego. Ganaste " + victorias + " veces y perdiste " + derrotas + " veces." )
