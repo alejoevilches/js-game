e1ec3e50e67891d746edfef564b5f5b93b8c4151
alejoevilches <alejoevilches@gmail.com>
2022-12-11 16:51:39 -0300

    Starting player selection section

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-//Inicializo variables
-let jugador
-let victorias = 0
-let derrotas = 0
+//Creacion de variables
+let selectPlayerButton=document.getElementById("player_selectbutton");
+let bounou=document.getElementById("bounou");
+let mbappe=document.getElementById("mbappe");
+let neymar=document.getElementById("neymar");
+let enzo=document.getElementById("enzo");
+let maguire=document.getElementById("maguire");
+let jordi=document.getElementById("jordi");
 
-//Funcion para que la PC elija un numero
-function range(min, max){
-    return Math.floor(Math.random()*(max-min+1))+min;}
-
-//Funcion para que se muestre en pantalla la eleccion de las partes
-function election(val){
-    if (val==1){
-        return "Piedra";
-    } else if (val==2){
-        return "Papel";
-    } else if (val==3){
-        return "Tijera";
+//Funcion para confirmar selección del jugador
+function handleConfifmPlayerSelection(){
+    if (bounou.checked==true){
+        alert("Seleccionaste a Yassine Bounou")
     }
+    else if (mbappe.checked==true){
+        alert("Seleccionaste a Kylian Mbappe")
     }
-
-function count(pc, jugador){
-    if (jugador == pc){
-        alert("EMPATE");
-    } else if ((jugador==1 && pc==3) || (jugador==2 && pc==1) || (jugador==3 && pc==2)){
-        alert("GANASTE");
-        victorias++
-    } else{
-        alert("PERDISTE");
-        derrotas++
+    else if (neymar.checked==true){
+        alert("Seleccionaste a Neymar")
+    }
+    else if (enzo.checked==true){
+        alert("Seleccionaste a Enzo Fernandez")
+    }
+    else if (maguire.checked==true){
+        alert("Seleccionaste a Harry Maguire")
+    }
+    else if (jordi.checked==true){
+        alert("Seleccionaste a Jordi Alba")
+    }
+    else{
+        alert("Debes seleccionar una opción para poder continuar")
     }
 }
 
-//Hago el bucle para que se repita hasta que gane el mejor de 3
-while (victorias < 3 && derrotas < 3){
-    let pc=range(1,3);
-    let jugador=prompt("1 es piedra, 2 es papel, 3 es tijera");
-    alert("El jugador eligió " + election(jugador))
-    alert("La PC eligió " + election(pc))
-    count(pc, jugador);
-}
-alert("Fin del juego. Ganaste " + victorias + " veces y perdiste " + derrotas + " veces." )
+//Event listener para ejecutar confirmación del jugador
+selectPlayerButton.addEventListener("click", handleConfifmPlayerSelection);
+
