22296b7a2e63bf7735252e819f6c56563677b219
alejoevilches <alejoevilches@gmail.com>
2022-12-16 16:52:50 -0300

    first commit. import from github

--- script.js
+++ script.js
@@ -1,154 +1,167 @@
-//Creacion de variables globales para botones y otros
-let selectPlayerButton=document.getElementById("player_selectbutton");
-let shoot=document.getElementById("shoot_button");
-let speed=document.getElementById("speed_button");
-let defense=document.getElementById("defense_button");
-let restartButton=document.getElementById("restart_button")
-let playersList=["Yassine Bounou", "Kylian Mbappe", "Neymar", "Enzo Fernandez", "Harry Maguire", "Luis Suarez"]
-let attackList=["Disparo", "Defensa", "Velocidad"];
-let attackSection=document.getElementById("attack_section")
-let playerCurrentLives=3
-let pcCurrentLives=3
-let playerAttackSelection
-let pcAttackSelection
-let result
+let ataqueJugador
+let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
 
-//Creacion de variables de los elementos dinámicos del DOM
-let playerLives=document.getElementById("player_lives");
-let pcLives=document.getElementById("pc_lives");
-let p1Player=document.getElementById("p1_player")
-let pcPlayer=document.getElementById("pc_player")
-let pcAttack=document.getElementById("pc_attack");
-let resultAlert=document.getElementById("result");
+function iniciarJuego() {
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'none'
 
-function changeDisplay(section){
-    section.style.display="block";
+    let sectionReiniciar = document.getElementById('reiniciar')
+    sectionReiniciar.style.display = 'none'
+    
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.addEventListener('click', ataqueFuego)
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.addEventListener('click', ataqueAgua)
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.addEventListener('click', ataqueTierra)
+
+    let botonReiniciar = document.getElementById('boton-reiniciar')
+    botonReiniciar.addEventListener('click', reiniciarJuego)
 }
 
-//Funcion para crear numeros aleatorios
-function random(min, max){
-    Math.floor(Math.random()*(max-min+1))+min;
-}
-
-//Funcion para confirmar selección del jugador
-function handleConfifmPlayerSelection(){
-    let bounou=document.getElementById("bounou");
-    let mbappe=document.getElementById("mbappe");
-    let neymar=document.getElementById("neymar");
-    let enzo=document.getElementById("enzo");
-    let maguire=document.getElementById("maguire");
-    let suarez=document.getElementById("suarez");
-    if (bounou.checked==true){
-        alert("Seleccionaste a Yassine Bounou")
-        p1Player.innerText="Yassine Bounou";
-    }
-    else if (mbappe.checked==true){
-        alert("Seleccionaste a Kylian Mbappe")
-        p1Player.innerHTML="Kylian Mbappe";
-    }
-    else if (neymar.checked==true){
-        alert("Seleccionaste a Neymar")
-        p1Player.innerHTML="Neymar";
-    }
-    else if (enzo.checked==true){
-        alert("Seleccionaste a Enzo Fernandez")
-        p1Player.innerHTML="Enzo Fernandez";
-    }
-    else if (maguire.checked==true){
-        alert("Seleccionaste a Harry Maguire")
-        p1Player.innerHTML="Harry Maguire";
-    }
-    else if (suarez.checked==true){
-        alert("Seleccionaste a Luis Suarez")
-        p1Player.innerHTML="Luis Suarez";
-    }
-    else{
-        alert("Debes seleccionar una opción para poder continuar");
-    }
-    pcPlayerSelection();
-    attackSelect();
-}
-
-//Función para que la PC elija un jugador
-function pcPlayerSelection(){
-    let selection=Math.floor(Math.random()*(5-0+1))+0;
-    pcPlayer.innerText=playersList[selection];
-}
-
-
-//Función para seleccionar el ataque del jugador y ejecutar la batalla
-function attackSelect(){
-    defense.addEventListener("click", defenseSelection);
-    speed.addEventListener("click", speedSelection);
-    shoot.addEventListener("click", shootSelection)
-    let playerAttack=document.getElementById("player_attack");
-
-    function defenseSelection(){
-        playerAttackSelection="Defensa"
-        playerAttack.innerHTML=playerAttackSelection;
-        pcAttackSelect();
-        pcAttack.innerHTML=pcAttackSelection
-        battle();
-    }
-
-    function speedSelection(){
-        playerAttackSelection="Velocidad"
-        playerAttack.innerHTML=playerAttackSelection;
-        pcAttackSelect();
-        pcAttack.innerHTML=pcAttackSelection
-        battle();
-    }
-
-    function shootSelection(){
-        playerAttackSelection="Disparo"
-        playerAttack.innerHTML=playerAttackSelection;
-        pcAttackSelect();
-        pcAttack.innerHTML=pcAttackSelection
-        battle();
-    }
-}
-
-//Funcion para seleccionar el ataque de la PC
-function pcAttackSelect(){
-    let selection=Math.floor(Math.random()*(2-0+1))+0;
-    pcAttackSelection=attackList[selection];
-}
-
-//Función para reiniciar el juego
-function restartGame(){
-    location.reload();
-}
-
-/* Función de batallas entre personajes. Tambien alerta si un jugador ganó o perdió
-MPORTANTE: En este juego Defensa le gana a Velocidad, Disparo le gana a Defensa y Velocidad le gana a Disparo */
-function battle(){
-    if (playerAttackSelection === pcAttackSelection){
-        result="EMPATE" 
-    }  else if ((playerAttackSelection=="Defensa" && pcAttackSelection=="Velocidad") || (playerAttackSelection=="Disparo" && pcAttackSelection=="Defensa") || (playerAttackSelection=="Velocidad" && pcAttackSelection=="Disparo")) {
-        result="GANASTE";
-        pcCurrentLives--
-        pcLives.innerHTML=pcCurrentLives;
+function seleccionarMascotaJugador() {
+    let sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+    sectionSeleccionarMascota.style.display = 'none'
+    
+    let sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+    sectionSeleccionarAtaque.style.display = 'flex'
+    
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
     } else {
-        result="PERDISTE";
-        playerCurrentLives--
-        playerLives.innerHTML=playerCurrentLives;
+        alert('Selecciona una mascota')
     }
-    resultAlert.innerText=result;
 
-    if (pcCurrentLives==0){
-        alert("GANASTE EL JUEGO!")
-        reset.style.display="block";
-        defense.disabled=true; 
-        shoot.disabled=true;
-        speed.disabled=true;
-        } else if (playerCurrentLives==0) { 
-        alert("PERDISTE")
-        reset.style.display="block";
-        defense.disabled=true; 
-        shoot.disabled=true;
-        speed.disabled=true;
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let mascotaAleatoria = aleatorio(1,3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    if (mascotaAleatoria == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+    } else if (mascotaAleatoria == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+    } else {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
     }
 }
 
-//Event listeners para ejecutar confirmaciónes del jugador
-selectPlayerButton.addEventListener("click", handleConfifmPlayerSelection);
+function ataqueFuego() {
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+    
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+    }
+
+    combate()
+}
+
+function combate() {
+    let spanVidasJugador = document.getElementById('vidas-jugador')
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+    
+    if(ataqueEnemigo == ataqueJugador) {
+        crearMensaje("EMPATE")
+    } else if(ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else if(ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else if(ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA') {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else {
+        crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+
+    revisarVidas()
+}
+
+function revisarVidas() {
+    if (vidasEnemigo == 0) {
+        crearMensajeFinal("FELICITACIONES! Ganaste :)")
+    } else if (vidasJugador == 0) {
+        crearMensajeFinal('Lo siento, perdiste :(')
+    }
+}
+
+function crearMensaje(resultado) {
+    let sectionMensajes = document.getElementById('resultado')
+    let ataquesDelJugador = document.getElementById('ataques-del-jugador')
+    let ataquesDelEnemigo = document.getElementById('ataques-del-enemigo')
+    
+    let nuevoAtaqueDelJugador = document.createElement('p')
+    let nuevoAtaqueDelEnemigo = document.createElement('p')
+
+    sectionMensajes.innerHTML = resultado
+    nuevoAtaqueDelJugador.innerHTML = ataqueJugador
+    nuevoAtaqueDelEnemigo.innerHTML = ataqueEnemigo
+
+    ataquesDelJugador.appendChild(nuevoAtaqueDelJugador)
+    ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
+}
+
+function crearMensajeFinal(resultadoFinal) {
+    let sectionMensajes = document.getElementById('resultado')
+    
+    sectionMensajes.innerHTML = resultadoFinal
+
+    let botonFuego = document.getElementById('boton-fuego')
+    botonFuego.disabled = true
+    let botonAgua = document.getElementById('boton-agua')
+    botonAgua.disabled = true
+    let botonTierra = document.getElementById('boton-tierra')
+    botonTierra.disabled = true
+
+    let sectionReiniciar = document.getElementById('reiniciar')
+    sectionReiniciar.style.display = 'block'
+}
+
+function reiniciarJuego() {
+    location.reload()
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+window.addEventListener('load', iniciarJuego)
