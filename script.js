f92c56a7e1ab05182e701821608de23cc4dae65f
alejoevilches <alejoevilches@gmail.com>
2022-12-17 17:18:54 -0300

    creating card components

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //Variables del juego
-let ataqueJugador
-let ataqueEnemigo
-let vidasJugador = 3
-let vidasEnemigo = 3
+let ataqueJugador;
+let ataqueEnemigo;
+let opcionDeMokepones;
+let vidasJugador = 3;
+let vidasEnemigo = 3;
 let mokepones=[];
 
 //Constantes que incluyen botones de acciones
@@ -35,18 +36,46 @@ class Mokepon{
         this.nombre=nombre;
         this.imagen=imagen;
         this.vidas=vidas;
+        this.ataques=[];
     }     
 }
 
 
-//Creación de Mokepones 
+//Creación de Mokepones para agregarlos a su array
 let hipodoge=new Mokepon("Hipodoge", "/img/hipodoge.png", 3);
+hipodoge.ataques.push=[
+    {nombre:"Ataque acuatico", id:"boton-agua"},
+    {nombre:"Bombero voluntario", id:"boton-agua"},
+    {nombre:"Manguerazo poderoso", id:"boton-agua"},
+    {nombre:"Incendio", id:"boton-fuego"},
+    {nombre:"Terremoto", id:"boton-tierra"},
+]
+
 let capipego=new Mokepon("Capipego", "/img/capipego.png", 3);
+capipego.ataques.push=[
+    {nombre:"Terremoto", id:"boton-tierra"},
+    {nombre:"Bola de tierra", id:"boton-tierra"},
+    {nombre:"Monticulo asesino", id:"boton-tierra"},
+    {nombre:"Incendio", id:"boton-fuego"},
+    {nombre:"Tsunami", id:"boton-agua"},
+]
+
 let ratigueya=new Mokepon("Ratigueya", "/img/ratigueya.png", 3);
+ratigueya.ataques.push=[
+    {nombre:"Incendio", id:"boton-fuego"},
+    {nombre:"Horno furioso", id:"boton-fuego"},
+    {nombre:"Encendedor", id:"boton-fuego"},
+    {nombre:"Terremoto", id:"boton-tierra"},
+    {nombre:"Tsunami", id:"boton-agua"},
+]
+
 mokepones.push(hipodoge,capipego,ratigueya);
 
 function iniciarJuego() {
     sectionSeleccionarAtaque.style.display = 'none'
+    mokepones.forEach((mokepon) =>{
+        console.log(mokepon);
+    })
     sectionReiniciar.style.display = 'none'
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
     botonFuego.addEventListener('click', ataqueFuego)
